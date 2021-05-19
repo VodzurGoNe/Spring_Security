aa8dc028ce35716edef8cc91af80724b7c31c6d9
Vladislav Gruzdov <gruzdov.v.1991@hotmail.com>
2021-05-19 21:03:35 +0400

    Реализована работа с БД

--- spring_course_security/src/main/java/org/gruzdov/spring/security/configuration/MyConfig.java
+++ spring_course_security/src/main/java/org/gruzdov/spring/security/configuration/MyConfig.java
@@ -1,18 +1,49 @@
 package org.gruzdov.spring.security.configuration;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 
 @Configuration
 @ComponentScan({"org.gruzdov.spring.security"})
 @EnableWebMvc
+@PropertySource(value = "classpath:db.property")
 public class MyConfig {
+    private Environment environment;
+
+    @Autowired
+    public void Environment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        try {
+            dataSource.setDriverClass(environment.getRequiredProperty("db.driverClassName"));
+            dataSource.setJdbcUrl(environment.getRequiredProperty("db.url"));
+            dataSource.setUser(environment.getRequiredProperty("db.username"));
+            dataSource.setPassword(environment.getRequiredProperty("db.password"));
+
+        } catch (PropertyVetoException var3) {
+            var3.printStackTrace();
+        }
+
+        return dataSource;
+    }
 
     @Bean
     public ViewResolver viewResolver() {
--- spring_course_security/src/main/java/org/gruzdov/spring/security/configuration/MySecurityConfig.java
+++ spring_course_security/src/main/java/org/gruzdov/spring/security/configuration/MySecurityConfig.java
@@ -1,27 +1,25 @@
 package org.gruzdov.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    private DataSource dataSource;
+
+    @Autowired
+    public void DataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("zohan")
-                        .password("zohan")
-                        .roles("EMPLOYEE"))
-                .withUser(userBuilder.username("phantom")
-                        .password("phantom")
-                        .roles("HR"))
-                .withUser(userBuilder.username("temp")
-                        .password("temp")
-                        .roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
